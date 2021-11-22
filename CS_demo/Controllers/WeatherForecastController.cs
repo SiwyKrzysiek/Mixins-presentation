2502957a6f7ccd3716046d810d21095fa380b314
Krzysztof Dąbrowski <krzysztof.dabrowski@predicagroup.com>
2021-11-22 16:53:49 +0100

    Add live demo code

---
+++ CS_demo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using CS_demo.mixins;
+
+namespace CS_demo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase, IShortMemory
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        this.Remember("aaa");
+
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ CS_demo/mixins/ShortMemoryMixin.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CS_demo.mixins;
+
+public interface IShortMemory
+{
+}
+
+internal class State
+{
+    public ISet<string> Memory { get; set; }
+}
+
+public static class ShortMemoryMixin
+{
+    private static readonly ConditionalWeakTable<IShortMemory, State> states = new();
+
+    public static void Remember(this IShortMemory instance, string id)
+    {
+        if (!states.TryGetValue(instance, out var state))
+        {
+            state = new State() { Memory = new HashSet<string>() };
+            states.Add(instance, state);
+        }
+
+        state.Memory.Add(id);
+    }
+
+    public static bool DoYouRemember(this IShortMemory instance, string id)
+    {
+        if (!states.TryGetValue(instance, out var state))
+        {
+            state = new State() { Memory = new HashSet<string>() };
+            states.Add(instance, state);
+        }
+
+        return state.Memory.Contains(id);
+    }
+}
